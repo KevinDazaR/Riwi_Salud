90c12d3e6cad044c5abd994d0611b225f25684a5
KevinDazaR <kevindaza13@gmail.com>
2024-04-23 20:53:37 -0500

    Creacion git ignore

--- obj/Debug/net8.0/RiwiSalud.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiwiSalud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiwiSalud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eabc04b92f28592f40841630e3fda1297feb91cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiwiSalud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiwiSalud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
