66b012431fd70cede50c7ea3e965896261ded3b3
KevinDazaR <kevindaza13@gmail.com>
2024-04-23 21:07:45 -0500

    Git Ignore setting

--- obj/Debug/net8.0/RiwiSalud.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiwiSalud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiwiSalud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8da978a73847f37a509dd2472d4c44acd7f3c31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+448e6f462877cb5ae0ed7d0fed2f47e6bf178ba1")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiwiSalud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiwiSalud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
