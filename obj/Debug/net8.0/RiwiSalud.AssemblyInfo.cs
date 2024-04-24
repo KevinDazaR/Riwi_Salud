35c143b656c7adc5db6dc1b54cd6c6135f6149c6
KevinDazaR <kevindaza13@gmail.com>
2024-04-24 15:35:49 -0500

    Try5

--- obj/Debug/net8.0/RiwiSalud.AssemblyInfo.cs
+++ obj/Debug/net8.0/RiwiSalud.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RiwiSalud")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a29e497f844c8245442ae00465cf6f93da53ced")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdb7cad41a50558d56001ffef4b23b2dc3ca8e30")]
 [assembly: System.Reflection.AssemblyProductAttribute("RiwiSalud")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RiwiSalud")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
