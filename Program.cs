c286c91ee64cebc667398eeb6fd23a12bddef4b6
KevinDazaR <kevindaza13@gmail.com>
2024-04-23 21:23:51 -0500

    Creaciòn de entorno y configuraciòn de MVC

--- Program.cs
+++ Program.cs
@@ -1,7 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RiwiSalud.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BaseContext>(options =>
+        options.UseMySql(
+            builder.Configuration.GetConnectionString("MySqlConnection"),
+            Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.20-mysql")
+        ));
+
 
 var app = builder.Build();
 
